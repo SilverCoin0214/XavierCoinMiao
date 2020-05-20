18e81e3cb38b6f9f68af3c66095305fa4999954c
zhangzuoxuan <zhangzuoxuan@gmail.com>
2020-05-20 17:37:33 +0800

    chunk

--- lodash/silvercoin0214-lodash.js
+++ lodash/silvercoin0214-lodash.js
@@ -1,9 +1,26 @@
 var silvercoin0214 = {
   isNull: function (val) {
-    if (val == null) {
+    if (val == null || val == undefined) {
       return true;
     } else {
       return false;
     }
   },
+
+  chunk: function (array, size = 1) {
+    var split_array = [];
+    for (var i = 0; i < size; i++) {
+      split_array.push(array[i]);
+    }
+    for (var i = 0; i < size; i++) {
+      array.shift(i);
+    }
+
+    var new_array = [];
+    new_array.push(split_array);
+    new_array.push(array);
+    return new_array;
+  },
 };
+
+console.log(silvercoin0214.chunk([1, 2, 3], 2));
