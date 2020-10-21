431bcdfc3169e7fb419e9dd67665a3376d863cb3
git <zhangzuoxuan@gmail.com>
2020-10-21 15:31:46 +0800

    postcss

--- webpack/webpack-react-scaffold/config/webpack.dev.config.js
+++ webpack/webpack-react-scaffold/config/webpack.dev.config.js
@@ -11,6 +11,23 @@ module.exports = merge(common, {
     filename: "js/[name].[hash:8].bundle.js",
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+
   // 配置开发测试时服务器的一些属性, 端口9000, 启动热更新, 自动打开
   devServer: {
     contentBase: path.resolve(__dirname, "../dist"),
